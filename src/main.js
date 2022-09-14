6e655818ae1d566242dc2eacdabd51d0a602fd0b
ppatkar <pspatkar@hotmail.com>
2022-09-14 19:33:22 +0530

    22-09-14

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import {
 	createApp
 } from "vue";
 
-import
-ScssApp
-from "./apps/ScssApp.vue";
+// import
+// ScssApp
+// from "./apps/ScssApp.vue";
+
+// import RouterApp from "./apps/RouterApp.vue";
+// import router from "./routes/router.js";
+
+import RootComp from "./apps/RootComp.vue";
 
 
-let app = createApp(ScssApp);
+let app = createApp(RootComp);
+//app.use(router);
+
 app.mount("#app");
